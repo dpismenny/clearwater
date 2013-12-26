93add302a2b729a78931a8247fbecf761f7c55c8
dpismenny <denis.pismenny@gmail.com>
2013-12-26 17:13:59 +0200

    header slide

--- js/common.js
+++ js/common.js
@@ -1,3 +1,66 @@
 $(document).ready(function() {
 
+	// .header height
+	(function() {
+		var height = $(window).height();
+				header = $('.header');
+
+		header.height(height);
+	})();
+
+	// slide-btn scroll
+	$('.js-slide').click(function (){
+		$('html, body').animate({
+			scrollTop: $('#numbers').offset().top
+		}, 500);
+	});
+
+
 });
+
+
+
+// window resize
+$(window).resize(function() {
+
+	// .header height
+	(function() {
+		var height = $(window).height();
+				header = $('.header');
+
+		header.height(height);
+	})();
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// (function() {
+	// 	var link = $('link');
+
+	// 	$('button').on('click', function() {
+	// 		var $this = $(this),
+	// 			stylesheet = $this.data('file');
+
+	// 		link.attr('href', stylesheet + '.css');
+
+	// 		$this
+	// 			.siblings('button')
+	// 				.removeAttr('disabled')
+	// 				.end()
+	// 			.attr('disabled', 'disabled');
+	// 	});
+	// })();
