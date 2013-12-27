72dc21df1b344d6abb9040e49582696ee9944953
dpismenny <denis.pismenny@gmail.com>
2013-12-27 17:24:25 +0200

    header footer

--- js/common.js
+++ js/common.js
@@ -13,8 +13,28 @@ $(document).ready(function() {
 		$('html, body').animate({
 			scrollTop: $('#numbers').offset().top
 		}, 500);
+		return false;
 	});
 
+
+	$('.js-popup-order').click(function (){
+		$('.popup_callback').hide();
+		$('.overlay').addClass('is-open');
+		$('.popup_order').slideToggle().toggleClass('is-open');
+	});
+
+	$('.js-popup-callback').click(function (){
+		$('.popup_order').hide();
+		$('.overlay').addClass('is-open');
+		$('.popup_callback').slideToggle().toggleClass('is-open');
+		return false;
+	});
+
+	$('.overlay').click(function (){
+		$(this).removeClass('is-open');
+		$('.popup').removeClass('is-open');
+		$('.popup').slideUp();
+	});
 
 });
 
