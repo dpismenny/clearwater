950d980894082115eecc33f7dffdd5b43e2da477
dpismenny <denis.pismenny@gmail.com>
2013-12-28 22:54:58 +0200

    up

--- js/common.js
+++ js/common.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 	//xPosition - Horizontal position of the element
 	//inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
 	//outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
-	// $('.header').parallax("50%", 0.4);
+	$('.header').parallax("50%", 0.4);
 	$('section.about').parallax("50%", 0.1);
 	$('section.production').parallax("50%", 0.1);
 	$('section.service').parallax("50%", 0.1);
