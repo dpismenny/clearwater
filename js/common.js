1765f01bb6a0d4be95c2ea08636d6e137b69b2c1
dpismenny <denis.pismenny@gmail.com>
2014-01-23 17:07:05 +0200

    on water.html icons fixed

--- js/common.js
+++ js/common.js
@@ -22,19 +22,19 @@
   }
 
 			  // 4. The API will call this function when the video player is ready.
-			  function onPlayerReady(event) {
-			    event.target.playVideo();
-			  }
+			  // function onPlayerReady(event) {
+			  //   event.target.playVideo();
+			  // }
 
 			  // 5. The API calls this function when the player's state changes.
 			  //    The function indicates that when playing a video (state=1),
 			  //    the player should play for six seconds and then stop.
 			  var done = false;
 			  function onPlayerStateChange(event) {
-			    if (event.data == YT.PlayerState.PLAYING && !done) {
-			      setTimeout(stopVideo, 6000);
-			      done = true;
-			    }
+			    // if (event.data == YT.PlayerState.PLAYING && !done) {
+			    //   setTimeout(stopVideo, 6000);
+			    //   done = true;
+			    // }
 			  }
 			  function stopVideo() {
 			    player.stopVideo();
@@ -118,7 +118,6 @@ $(document).ready(function() {
 		else {
 			$('.v-player').addClass('is-open');
 			$('.overlay').addClass('is-video-open');
-			//player.stopVideo();
 			player.playVideo();
 		}
 		return false;
